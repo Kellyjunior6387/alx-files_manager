af9da43bed5401a177c833ab2c92b6e1cf17c0c5
Kellyjunior6387 <kellyjunior6386@gmail.com>
2024-08-31 16:25:28 +0300

    task 2

--- utils/db.js
+++ utils/db.js
@@ -5,31 +5,24 @@ class DBClient {
     const host = process.env.DB_HOST || 'localhost';
     const port = process.env.DB_PORT || 27017;
     const database = process.env.DB_DATABASE || 'files-manager';
-    const uri = `mongodb://${host}:${port}`;
-    this.client = new MongoClient(uri);
-    this.connected = false;
+    const uri = `mongodb://${host}:${port}/${database}`;
+    this.client = new MongoClient(uri, { useUnifiedTopology: true });
     this.client.connect()
-      .then(() => {
-        this.database = this.client.db(database);
-        this.connected = true;
-      })
       .catch((err) => {
         console.error('Failed to connect to MongoDB:', err);
       });
   }
 
   async isAlive() {
-    return this.connected;
+    return this.client.isConnected();
   }
 
   async nbUsers() {
-    if (!this.connected) throw new Error('MongoClient is not connected');
-    return this.database.collection('users').countDocuments();
+    return this.client.db().collection('users').countDocuments();
   }
 
   async nbFiles() {
-    if (!this.connected) throw new Error('MongoClient is not connected');
-    return this.database.collection('files').countDocuments();
+    return this.client.db().collection('files').countDocuments();
   }
 }
 const dbClient = new DBClient();
